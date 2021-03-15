13763801a6fbaac0524261186ce1c10713c05963
kadotaoru <daiyaogata898@gmail.com>
2021-03-16 05:38:55 +0900

    made basic quiz app

--- app/src/main/java/to/msn/wings/forequiz/MainActivity.java
+++ app/src/main/java/to/msn/wings/forequiz/MainActivity.java
@@ -1,11 +1,12 @@
 package to.msn.wings.forequiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,43 +14,103 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] QUIZ = {"ジョナサン", "ジョセフ", "承太郎", "仗助"};
-    int tap = 0;
+    private TextView tvCount;
+    private TextView tvQuestion;
+    private Button ansBtn1;
+    private Button ansBtn2;
+    private Button ansBtn3;
+    private Button ansBtn4;
+    private Button nextBtn;
+
+    private int i = 0;
+
+    String[][] quizData = {
+            // {"問題", "正解", "選択肢１", "選択肢２", "選択肢３"}
+            {"問題A", "A1", "A2", "A3", "A4"},
+            {"問題B", "B1", "B2", "B3", "B4"},
+            {"問題C", "C1", "C2", "C3", "C4"},
+            {"問題D", "D1", "D2", "D3", "D4"},
+            {"問題E", "E1", "E2", "E3", "E4"}
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //選択肢のシャッフル
-        List<String> list = Arrays.asList(QUIZ.clone());
-        Collections.shuffle(list);
-        ((Button)findViewById(R.id.b0)).setText(list.get(0));
-        ((Button)findViewById(R.id.b1)).setText(list.get(1));
-        ((Button)findViewById(R.id.b2)).setText(list.get(2));
-        ((Button)findViewById(R.id.b3)).setText(list.get(3));
+        getId();
+
+        showQuiz();
+
     }
 
-    public void onButton(View v){
-        String text = ((Button)v).getText().toString();
+    public void getId(){
+        tvCount = findViewById(R.id.tvCount);
+        tvQuestion = findViewById(R.id.tvQuestion);
+        ansBtn1 = findViewById(R.id.ansBtn1);
+        ansBtn2 = findViewById(R.id.ansBtn2);
+        ansBtn3 = findViewById(R.id.ansBtn3);
+        ansBtn4 = findViewById(R.id.ansBtn4);
+        nextBtn = findViewById(R.id.nextBtn);
+    }
 
-        //正解
-        if(text.equals(QUIZ[tap])){
-            v.setEnabled(false);
-            tap++;
-            ((TextView)findViewById(R.id.tv)).setText( tap+"問正解！！");
-        //全問正解
-            if(tap >= 4){
-                ((TextView)findViewById(R.id.tv)).setText("ゲームクリア");
+    //クイズを表示
+    public void showQuiz(){
+        List<Integer> num = Arrays.asList(1, 2, 3, 4);
+        Collections.shuffle(num);
+
+        tvCount.setText("残り" + (5-i) + "問");
+        tvQuestion.setText(quizData[i][0]);
+        ansBtn1.setText(quizData[i][num.get(0)]);
+        ansBtn2.setText(quizData[i][num.get(1)]);
+        ansBtn3.setText(quizData[i][num.get(2)]);
+        ansBtn4.setText(quizData[i][num.get(3)]);
+    }
+
+    public void onButton(View view){
+        //クリックされたボタン
+        Button clickedBtn = (Button)view;
+        String clickedAns = clickedBtn.getText().toString();  //クリックされたボタンの文字列
+
+        if(clickedAns.equals(quizData[i][1])) {
+            clickedBtn.setText("正解！");
+            //ボタンを無効化、ネクストボタンを有効化
+            ansBtn1.setEnabled(false);
+            ansBtn2.setEnabled(false);
+            ansBtn3.setEnabled(false);
+            ansBtn4.setEnabled(false);
+            nextBtn.setEnabled(true);
+
+            //５問正解したら;
+            if (i == 4) {
+                //画面遷移
+                Intent intent = new Intent(this, Result.class);
+                startActivity(intent);
+                finish();
+                } else {
+                    i++;
             }
-
         } else {
-            ((TextView)findViewById(R.id.tv)).setText("ゲームオーバー");
-            ((Button)findViewById(R.id.b0)).setEnabled(false);
-            ((Button)findViewById(R.id.b1)).setEnabled(false);
-            ((Button)findViewById(R.id.b2)).setEnabled(false);
-            ((Button)findViewById(R.id.b3)).setEnabled(false);
+            clickedBtn.setText("不正解！");
+            tvQuestion.setText("Game over");
+
+            //ボタンを無効化
+            ansBtn1.setEnabled(false);
+            ansBtn2.setEnabled(false);
+            ansBtn3.setEnabled(false);
+            ansBtn4.setEnabled(false);
+            nextBtn.setEnabled(false);
         }
     }
 
+    //Nextボタンが押された時の処理
+    public void onNext(View view){
+        showQuiz();
+
+        ansBtn1.setEnabled(true);
+        ansBtn2.setEnabled(true);
+        ansBtn3.setEnabled(true);
+        ansBtn4.setEnabled(true);
+    }
 }
