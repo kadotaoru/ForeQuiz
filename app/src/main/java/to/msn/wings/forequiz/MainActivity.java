11f6d32229dac8155a04c9e5b9673822fad3dcf9
kadotaoru <daiyaogata898@gmail.com>
2021-03-16 05:48:21 +0900

    Add full screen mode

--- app/src/main/java/to/msn/wings/forequiz/MainActivity.java
+++ app/src/main/java/to/msn/wings/forequiz/MainActivity.java
@@ -2,6 +2,7 @@ package to.msn.wings.forequiz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -43,6 +44,41 @@ public class MainActivity extends AppCompatActivity {
 
         showQuiz();
 
+    }
+
+    //全ステータスバーを非表示にしてフルスクリーン化
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        // Note that system bars will only be "visible" if none of the
+                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            Log.d("debug","The system bars are visible");
+                        } else {
+                            Log.d("debug","The system bars are NOT visible");
+                        }
+                    }
+                });
     }
 
     public void getId(){
